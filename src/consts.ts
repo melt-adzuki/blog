86c436eb5d8e7e92134eadec302c5aa6f08c4870
あずき⪥™ <44765629+melt-adzuki@users.noreply.github.com>
2025-12-05 20:29:46 +0900

    Edit Footer

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "紅豆新聞WEB";
-export const SITE_CREDIT = "ANW Technologies";
+export const SITE_CREDIT = "あずきゃ";
 export const SITE_DESCRIPTION =
     "あずきゃがお送りする「紅豆新聞WEB」。日常に迫る様々な出来事をお届けします。";
